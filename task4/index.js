b0d4d86f0ef71ef647a5e6e34a0db6268cbe8216
OdanetsLiliya <mashcovcheva@gmail.com>
2018-03-02 22:07:27 +0300

    add task4/index.js

---
+++ task4/index.js
@@ -0,0 +1,443 @@
+var photoPosts = [
+  {
+    id: '1',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23'),
+    author: 'Иванов Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '2',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23'),
+    author: 'Иванов Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '3',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '4',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-03-23'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '5',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '6',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '7',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '8',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '9',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '10',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-01-11'),
+    author: 'lily',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '11',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-01-11'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '12',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'lily',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '13',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-01-11'),
+    author: 'lily',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '14',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-01-11'),
+    author: 'lily',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#kek', '#summer', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '15',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '16',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#summer', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '17',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '18',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-01-11'),
+    author: 'lily',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#kek'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '19',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  },
+  {
+    id: '20',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-02-20'),
+    author: 'ivan',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#hashtag', '#summer', '#kek', '#day'],
+    likes: ['nick_name1', 'nick_name2']
+  }
+];
+var PostForAddWrongDescription = {
+  id: '21',
+  description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!' +
+  'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!' +
+  'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+  createdAt: new Date('2018-02-22'),
+  author: 'ivan',
+  photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+  hashtags: ['#summer', '#kek'],
+  likes: ['nick_name1']
+};
+var PostForAddWithoutHashtags = {
+  id: '22',
+  description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+  createdAt: new Date('2018-02-22'),
+  author: 'ivan',
+  photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+  likes: ['nick_name1']
+};
+
+var PostForAddWithoutRequiredFields = {
+  description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+  createdAt: new Date('2018-02-22'),
+  photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+  likes: ['nick_name1']
+}
+
+var PostForAddWithSameID = {
+  id: '22',
+  description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+  createdAt: new Date('2018-02-22'),
+  author: 'ivan',
+  photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+  likes: ['nick_name1']
+}
+
+var PostForAddWithWrongTypeOfFields = {
+  id: 45,
+  description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+  createdAt: new Date('2018-02-22'),
+  author: 808,
+  photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+  likes: ['nick_name1']
+}
+
+let modul = (function () {
+
+  photoPosts.sort(compareByDate);
+
+  function compareByDate(photoPostA, photoPostB) {
+    return Date.parse(photoPostB.createdAt) - Date.parse(photoPostA.createdAt);
+  }
+
+  function validTypeOfArray(array) {
+    if (Array.isArray(array)) {
+      return array.every(function (item) {
+        return typeof (item) == 'string';
+      });
+    }
+    return false;
+  }
+
+  let getPhotoPosts = function (skip, top, filterConfig) {
+    let posts = photoPosts;
+    if (typeof (skip) != 'number' || typeof (top) != 'number') {
+      return [];
+    }
+    if (filterConfig) {
+      if (filterConfig.author && (typeof (filterConfig.author) != 'string' || filterConfig.author.length == 0) ||
+        filterConfig.createdAt && !filterConfig.createdAt instanceof Date ||
+        filterConfig.hashtags && !validTypeOfArray(filterConfig.hashtags)) {
+        return [];
+      }
+      if (filterConfig.author) {
+        posts = posts.filter(function (item) {
+          return item.author === filterConfig.author;
+        });
+      }
+
+      if (filterConfig.createdAt) {
+        posts = posts.filter(function (item) {
+          return Date.parse(item.createdAt) == Date.parse(filterConfig.createdAt);
+        });
+      }
+
+      if (filterConfig.hashtags) {
+        posts = posts.filter(function (postItem) {
+          if (typeof (postItem.hashtags) == 'undefined') {
+            return false;
+          }
+          return filterConfig.hashtags.every(function (item) {
+            return postItem.hashtags.includes(item);
+          })
+        })
+      }
+    }
+    posts = posts.slice(skip, skip + top);
+    return posts;
+  };
+
+  let getPhotoPost = function (id) {
+    return photoPosts.find(item => item.id == id);
+  };
+
+  let addPhotoPost = function (photoPost) {
+    if (validatePhotoPost(photoPost)) {
+      photoPosts.push(photoPost);
+      photoPosts.sort(compareByDate);
+      return true;
+    }
+    return false;
+  };
+
+  let validatePhotoPost = function (photoPost, statusOfValidation) {
+    if (!photoPost) {
+      return false;
+    }
+
+    if (photoPost.description && typeof (photoPost.description) == 'string' &&
+      photoPost.description.length <= 200 &&
+      photoPost.photoLink && typeof (photoPost.photoLink) == 'string' && photoPost.photoLink.length != 0 &&
+      (typeof (photoPost.hashtags) == 'undefined' || validTypeOfArray(photoPost.hashtags)) &&
+      (statusOfValidation ||
+        photoPost.id && photoPosts.findIndex(item => item.id == photoPost.id) == -1 &&
+        typeof (photoPost.id) == 'string' &&
+        photoPost.Date && photoPost.createdAt instanceof Date &&
+        typeof (photoPost.author) == 'string' && photoPost.author.length != 0 &&
+        (typeof (photoPost.likes) == 'undefined' || validTypeOfArray(photoPost.likes))
+      )
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  let editPhotoPost = function (id, photoPost) {
+    let photopostToChange = this.getPhotoPost(id);
+    if (typeof (photopostToChange) == 'undefined') {
+      return false;
+    }
+    if (photoPost.description) {
+      photopostToChange.description = photoPost.description;
+    }
+    if (photoPost.photoLink) {
+      photopostToChange.photoLink = photoPost.photoLink;
+    }
+    if (photoPost.hashtags) {
+      photopostToChange.hashtags = photoPost.hashtags;
+    }
+    if (validatePhotoPost(photopostToChange, 'changes')) {
+      photoPosts[photoPosts.findIndex(item => item.id == id)] = photopostToChange;
+      return true;
+    }
+    return false;
+  };
+
+  let removePhotoPost = function (id) {
+    let index = photoPosts.findIndex(item => item.id == id);
+    if (index != -1) {
+      photoPosts.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  return {
+    getPhotoPost: getPhotoPost,
+    getPhotoPosts: getPhotoPosts,
+    addPhotoPost: addPhotoPost,
+    editPhotoPost: editPhotoPost,
+    removePhotoPost: removePhotoPost
+  }
+})();
+
+console.log('array of photoposts:')
+console.log(photoPosts);
+
+console.log('check mehtod getPhotoPost');
+console.log('getPhotopost(3)');
+console.log(modul.getPhotoPost(3));
+console.log('getPhotopost(2)');
+console.log(modul.getPhotoPost(2));
+console.log('getPhotopost(100) no such id');
+console.log(modul.getPhotoPost('100'));
+console.log("getPhotopost(['1', '4']) wrong type of arg");
+console.log(modul.getPhotoPost(['1', '4']));
+console.log('getPhotopost() no args');
+console.log(modul.getPhotoPost());
+
+console.log('check method remove photopost');
+console.log('removePhotopost(2)');
+console.log(modul.removePhotoPost('2'));
+console.log('removePhotopost(7)');
+console.log(modul.removePhotoPost('7'));
+console.log('removePhotopost(200) no such ID');
+console.log(modul.removePhotoPost(200));
+console.log('removePhotopost([1, 1]) wrong type of arg');
+console.log(modul.removePhotoPost([1, 1]));
+console.log('removePhotopost() no args');
+console.log(modul.removePhotoPost());
+
+console.log('array of photoposts after:')
+console.log(photoPosts);
+
+console.log('check method addPhotoPost');
+console.log('post with wrong description');
+console.log(modul.addPhotoPost(PostForAddWrongDescription));
+console.log('post without hashtags');
+console.log(modul.addPhotoPost(PostForAddWithoutHashtags));
+console.log('post without required fields');
+console.log(modul.addPhotoPost(PostForAddWithoutRequiredFields));
+console.log('post with same id');
+console.log(modul.addPhotoPost(PostForAddWithSameID));
+console.log('post with wrong type of fields');
+console.log(modul.addPhotoPost(PostForAddWithWrongTypeOfFields));
+console.log('no args');
+console.log(modul.addPhotoPost());
+
+console.log('array of photoposts after:')
+console.log(photoPosts);
+
+console.log('check method editPhotoPost');
+console.log("edit id=1 description: 'new description', hashtags: [2, '#summer']");
+console.log(modul.editPhotoPost('1', { description: 'new description', hashtags: [2, '#summer'] }));
+console.log("edit id=1 description: 2, hashtags: [2, '#summer']");
+console.log(modul.editPhotoPost('1', { description: 2, hashtags: [2, '#summer'] }));
+console.log("edit id=2(not exist) description: 'new description', hashtags: ['#summer']");
+console.log(modul.editPhotoPost('2', { description: 'new description', hashtags: ['#summer'] }));
+console.log("edit id=3 description: 'new description', hashtags: ['#summer']");
+console.log(modul.editPhotoPost('3', { description: 'new description', hashtags: ['#summer'] }));
+
+console.log('array of photoposts after:');
+console.log(photoPosts);
+
+console.log('check method getPhotoPosts');
+console.log('getPhotoPosts(3, 3):')
+console.log(modul.getPhotoPosts(3, 3));
+console.log('getPhotoPosts(10, 7):')
+console.log(modul.getPhotoPosts(10, 7));
+console.log("getPhotoPosts(1, 3,{ createdAt: new Date('2017-01-11') }):");
+console.log(modul.getPhotoPosts(1, 3, { createdAt: new Date('2017-01-11') }));
+console.log("wrong date getPhotoPosts(1, 3,{ createdAt: 'k' }):");
+console.log(modul.getPhotoPosts(1, 3, { createdAt: 'k' }));
+console.log("getPhotoPosts((0, 10, { createdAt: new Date('2017-01-11'), author: 'lily' })):");
+console.log(modul.getPhotoPosts(0, 10, { createdAt: new Date('2017-01-11'), author: 'lily' }));
+console.log("no such hashtag getPhotoPosts((0, 10, { hashtags: ['#h'] ");
+console.log(modul.getPhotoPosts(0, 10, { hashtags: ['#h'] }));
+console.log("getPhotoPosts((0, 10, { hashtags: ['#summer', '#day']");
+console.log(modul.getPhotoPosts(0, 10, { hashtags: ['#summer', '#day'] }));
+console.log('wrong type of hashtags getPhotoPosts((0, 10, { hashtags: 4 }');
+console.log(modul.getPhotoPosts(0, 10, { hashtags: 4 }));
+console.log('wrong type of author getPhotoPosts((0, 10, { author: [] }');
+console.log(modul.getPhotoPosts(0, 10, { author: [] }));
+console.log("getPhotoPosts((0, 10, { createdAt: new Date('2017-01-11'), author: 'lily', hashtags: ['#summer', '#day'] }");
+console.log(modul.getPhotoPosts(1, 3, { createdAt: new Date('2017-01-11'), author: 'lily', hashtags: ['#summer', '#day'] }));
+console.log("wrong skip/top getPhotoPosts((10, { hashtags: ['#summer', '#day'] }");
+console.log(modul.getPhotoPosts(10, { hashtags: ['#summer', '#day'] }));
+console.log("wrong skip/top getPhotoPosts(({ hashtags: ['#summer', '#day'] }");
+console.log(modul.getPhotoPosts({ hashtags: ['#summer', '#day'] }));
+console.log("wrong skip/top getPhotoPosts(('string', { hashtags: ['#summer', '#day'] }");
+console.log(modul.getPhotoPosts('string', { hashtags: ['#summer', '#day'] }));
